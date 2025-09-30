d39d775e320a3af25ddd2e81fee7e502b2d55f15
kibbles55 <kibbles.55.photic@icloud.com>
2025-09-30 23:52:44 +0300

    Update states.py

--- states.py
+++ states.py
@@ -15,9 +15,23 @@ class ViewPlans(StatesGroup):
     main = State()
     delete_plan = State()
     edit_plan = State()
+    chose_group = State()
+    add_exercises = State()
+    delete_exercises = State()
 
 class Settings(StatesGroup):
     main = State()
     add_exercises = State()
     change_name = State()
     muscle_choice = State()
+
+
+MUSCLE_GROUPS = [
+    {"id": 1, "name": "Груди"},
+    {"id": 2, "name": "Спина"},
+    {"id": 3, "name": "Дельти"},
+    {"id": 4, "name": "Трицепс"},
+    {"id": 5, "name": "Біцепс"},
+    {"id": 6, "name": "Ноги"},
+    {"id": 7, "name": "Живіт"},
+]
